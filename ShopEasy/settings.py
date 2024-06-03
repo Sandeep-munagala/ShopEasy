835799481126ca0c7e920c1a718ecf255d4fb9bc
tarunreddy127 <97306691+Sandeep-munagala@users.noreply.github.com>
2024-06-03 07:28:52 +0530

    messages

--- ShopEasy/settings.py
+++ ShopEasy/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     ]
 
-ROOT_URLCONF = "myproject.urls"
+ROOT_URLCONF = "ShopEasy.urls"
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "myproject.wsgi.application"
+WSGI_APPLICATION = "ShopEasy.wsgi.application"
 
 
 # Database
